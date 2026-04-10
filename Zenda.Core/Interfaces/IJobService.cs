08a9b9006803d7293cbb6d33233c5bea98b64423
ivanpaoloni@gmail.com <ivanpaoloni@gmail.com>
2026-04-10 09:36:44 -0300

    jobservice

---
+++ Zenda.Core/Interfaces/IJobService.cs
@@ -0,0 +1,10 @@
+﻿namespace Zenda.Core.Interfaces;
+
+public interface IJobService
+{
+    // Programa una tarea para el futuro y devuelve un ID (útil si después querés cancelar el turno)
+    string ProgramarRecordatorioEmail(string emailDestino, string nombreCliente, string nombreNegocio, DateTime fechaTurno, DateTime fechaEjecucion);
+
+    // Si un cliente cancela el turno, usamos esto para que no le llegue el mail
+    bool CancelarTrabajo(string jobId);
+}
