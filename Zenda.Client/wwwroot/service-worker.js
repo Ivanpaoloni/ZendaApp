371e8bd4665b6552bb5206b65b2d3c18c51e2868
ivanpaoloni@gmail.com <ivanpaoloni@gmail.com>
2026-05-04 01:29:56 -0300

    published

--- Zenda.Client/wwwroot/service-worker.js
+++ Zenda.Client/wwwroot/service-worker.js
@@ -1,18 +1,2 @@
-﻿// Cambia este número para forzar la actualización en los celulares
-const APP_VERSION = 'v1.0.9';
-
-self.addEventListener('install', event => {
-    console.log('Instalando SW:', APP_VERSION);
-    // 1. Instalar y no esperar a que se cierre la app
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-    console.log('Activando SW:', APP_VERSION);
-    // 2. Tomar el control de la pestaña abierta INMEDIATAMENTE
-    event.waitUntil(clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-    // Necesario para que Chrome lo reconozca como PWA válida
-});
+﻿// En desarrollo, simplemente pasamos de largo
+self.addEventListener('fetch', () => { });
---
+++ Zenda.Client/wwwroot/service-worker.published.js
@@ -0,0 +1,45 @@
+﻿// Este archivo es el que se compila y sube a Railway
+self.importScripts('./service-worker-assets.js');
+
+self.addEventListener('install', event => {
+    self.skipWaiting(); // TRUCO 1: No esperamos a nadie
+    event.waitUntil(onInstall(event));
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim().then(() => onActivate(event))); // TRUCO 2: Tomamos el control ya
+});
+
+self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
+
+const cacheNamePrefix = 'offline-cache-';
+const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
+const offlineAssetsInclude = [/\.dll$/, /\.wasm/, /\.html/, /\.js/, /\.json/, /\.css/, /\.woff/, /\.png/, /\.jpe?g/, /\.gif/, /\.ico/, /\.blat/, /\.dat/];
+const offlineAssetsExclude = [/^service-worker\.js$/];
+
+async function onInstall(event) {
+    console.info('Instalando nueva versión de Zendy...');
+    const assetsRequests = self.assetsManifest.assets
+        .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
+        .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
+        .map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
+    await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
+}
+
+async function onActivate(event) {
+    console.info('Activando nueva versión de Zendy...');
+    const cacheKeys = await caches.keys();
+    await Promise.all(cacheKeys
+        .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
+        .map(key => caches.delete(key)));
+}
+
+async function onFetch(event) {
+    let cachedResponse = null;
+    if (event.request.method === 'GET') {
+        const request = event.request;
+        const cache = await caches.open(cacheName);
+        cachedResponse = await cache.match(request);
+    }
+    return cachedResponse || fetch(event.request);
+}
