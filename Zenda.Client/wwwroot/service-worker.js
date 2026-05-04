aea30d85e1991c3b865ba39040e47e23e107801b
ivanpaoloni@gmail.com <ivanpaoloni@gmail.com>
2026-05-04 00:37:31 -0300

    worker

--- Zenda.Client/wwwroot/service-worker.js
+++ Zenda.Client/wwwroot/service-worker.js
@@ -1,1 +1,13 @@
-﻿
+﻿// Zendy Service Worker
+// IMPORTANTE: Cambia este número de versión cada vez que hagas un push 
+// y quieras que los usuarios vean el cartel de "Actualización".
+const APP_VERSION = 'v1.0.1';
+
+self.addEventListener('install', event => {
+    console.log('Instalando Service Worker versión:', APP_VERSION);
+    // No usamos skipWaiting() para dejar que el usuario decida cuándo recargar con el cartel
+});
+
+self.addEventListener('fetch', event => {
+    // Escucha básica para que los navegadores lo consideren una PWA válida
+});
