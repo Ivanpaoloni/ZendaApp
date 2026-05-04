1f208fb8bab784b8288dfdd0643bd8c03befe806
ivanpaoloni@gmail.com <ivanpaoloni@gmail.com>
2026-05-04 01:03:57 -0300

    actuacion silenciosa

--- Zenda.Client/wwwroot/service-worker.js
+++ Zenda.Client/wwwroot/service-worker.js
@@ -1,17 +1,11 @@
-﻿// Zendy Service Worker
-const APP_VERSION = 'v1.0.6'; // Subimos la versión para forzar el cambio
+﻿const APP_VERSION = 'v1.0.4'; // Cambia esto en cada push
 
 self.addEventListener('install', event => {
     console.log('Instalando Service Worker versión:', APP_VERSION);
+    // TRUCO: Toma el control inmediatamente, sin esperar
+    self.skipWaiting(); 
 });
 
 self.addEventListener('fetch', event => {
-    // Escucha básica
-});
-
-// NUEVO: Escuchamos el mensaje desde index.html para saltar la espera
-self.addEventListener('message', event => {
-    if (event.data && event.data.type === 'SKIP_WAITING') {
-        self.skipWaiting();
-    }
+    // Escucha básica para PWA
 });
